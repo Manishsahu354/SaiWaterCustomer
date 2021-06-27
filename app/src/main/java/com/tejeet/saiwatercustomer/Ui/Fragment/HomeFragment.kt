85220228eed93db4675da14df0b1fb297bcd7650
manishkumar <manishsahu354@gmail.com>
2021-06-27 15:19:57 +0530

    water customer

--- app/src/main/java/com/tejeet/saiwatercustomer/Ui/Fragment/HomeFragment.kt
+++ app/src/main/java/com/tejeet/saiwatercustomer/Ui/Fragment/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.tejeet.saiwatercustomer.Ui.Fragment
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.tejeet.saiwatercustomer.Ui.Activity.ManualOrderActivity
@@ -20,6 +22,9 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
+    internal val UPI_PAYMENT = 0
+
+
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private val mainViewModel: MainViewModel by viewModels()
@@ -31,6 +36,10 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         observeData()
+
+        binding.btnPaymentBill.setOnClickListener {
+            payUsingUpi("2000","tejeet@ybl","tejeet","Water Payment")
+        }
 
         binding.pullToRefresh.setOnRefreshListener {
             observeData()
@@ -86,6 +95,34 @@ class HomeFragment : Fragment() {
     fun map(x:Int, in_min:Int,  in_max:Int,  out_min:Int,  out_max:Int):Int
     {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    fun payUsingUpi(amount: String, upiId: String, name: String, note: String) {
+
+        val uri = Uri.parse("upi://pay").buildUpon()
+            .appendQueryParameter("pa", upiId)
+            .appendQueryParameter("pn", name)
+            .appendQueryParameter("tn", note)
+            .appendQueryParameter("am", amount)
+            .appendQueryParameter("cu", "INR")
+            .build()
+
+
+        val upiPayIntent = Intent(Intent.ACTION_VIEW)
+        upiPayIntent.data = uri
+
+        // will always show a dialog to user to choose an app
+        val chooser = Intent.createChooser(upiPayIntent, "Pay with")
+        startActivityForResult(chooser, UPI_PAYMENT)
+
+
+        // check if intent resolves
+//        if (null != chooser.resolveActivity(packageManager)) {
+//            startActivityForResult(chooser, UPI_PAYMENT)
+//        } else {
+//            Toast.makeText(requireContext(), "No UPI app found, please install one to continue", Toast.LENGTH_SHORT).show()
+//        }
+
     }
 
     override fun onDestroyView() {
